f6c2714935b6280206b64492158abd5cfff7b3b9
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-11 15:04:05 -0700

    add test for postReducer w ADD_POST

---
+++ src/reducers/postReducer.test.js
@@ -0,0 +1,22 @@
+import reducer from './postReducer';
+
+describe('postReducer', () => {
+  it('handles the ADD_POST ACTION', () => {
+    const state = [];
+    const action = addPost({
+        title: 'Read It Here First',
+        author: 'Boss Of You',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    });
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([
+      {
+        title: 'Read It Here First',
+        author: 'Boss Of You',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }
+    ]);
+  });
+});
