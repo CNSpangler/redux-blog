d0149992ace06b6da7be88077a61bb606d7eeda6
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-11 15:12:07 -0700

    test for deletePost action

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,6 +1,11 @@
 export const ADD_POST = 'ADD_POST';
-
 export const addPost = post => ({
   type: ADD_POST,
   payload: post
+});
+
+export const DELETE_POST = 'DELETE_POST';
+export const deletePost = index => ({
+  type: DELETE_POST,
+  payload: index
 });
--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,4 +1,4 @@
-import { ADD_POST, addPost } from '../actions/postActions';
+import { ADD_POST, addPost, DELETE_POST, deletePost } from '../actions/postActions';
 
 describe('add post', () => {
   it('creates an ADD_POST action', () => {
@@ -7,6 +7,7 @@ describe('add post', () => {
       author: 'Boss Of You',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     });
+
     expect(action).toEqual({
       type: ADD_POST,
       payload: {
@@ -14,6 +15,15 @@ describe('add post', () => {
         author: 'Boss Of You',
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }
+    });
+  });
+
+  it('creates a DELETE_POST action', () => {
+    const action = deletePost(3);
+
+    expect(action).toEqual({
+      type: DELETE_POST,
+      payload: 6
     });
   });
 });
